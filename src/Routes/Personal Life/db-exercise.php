1b4aad58b29a3752a6c51fcd50e01cd6376f5ee6
Vayre047 <94454907+Vayre047@users.noreply.github.com>
2022-06-14 18:37:30 +0100

    Implementação de imgens de hobbies no Personal Life Component

---
+++ src/Routes/Personal Life/db-exercise.php
@@ -0,0 +1,5 @@
+<?php
+	$db = new PDO("mysql:host=localhost;dbname=classicmodels;charset=utf8mb4", "root", "");
+
+	var_dump($db);
+?>
--- src/Routes/Personal Life/personal-life-component.jsx
+++ src/Routes/Personal Life/personal-life-component.jsx
@@ -1,7 +1,24 @@
 function PersonalLife() {
     return (
         <div>
-         
+            <p className="h1 text-center">Personal Life</p>
+            <div className="container">
+                <p className="h2 text-center pt-3">Hobbies</p>
+                <div className="row text-center pt-5">
+                    <div className="col-sm">
+                        <img src="../../Images/strava-icon.png" alt="Strava Icon" />
+                    </div>
+                    <div className="col-sm">
+                        <img src="../../Images/running-icon.png" alt="Running Icon" />
+                    </div>
+                    <div className="col-sm">
+                        <img src="../../Images/travelling-icon.png" alt="Travelling Icon" />
+                    </div>
+                    <div className="col-sm">
+                        <img src="../../Images/netflix-icon.png" alt="NetFlix Icon" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
